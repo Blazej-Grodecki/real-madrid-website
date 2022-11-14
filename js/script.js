548944fa1b9b1f1412539ef18d40ab5eaa61427c
Blazej Grodecki <b.grodecki@gmail.com>
2022-11-14 12:24:04 +0100

    added function in js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-console.log("Hello World! Ten kod został już wysłany do repozytorium Git.")
-
-let button = document.querySelector(".js-changeTheme");
-let body = document.querySelector(".body");
-let blueTheme = document.querySelector(".js-blueTheme");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-if (body.classList.contains("dark")) {
-    blueTheme.innerText = "Szary";
-  } else {
-    blueTheme.innerText = "Niebieski";
+{
+  const welcome = () => {
+    console.log("Hello World! Ten kod został już wysłany do repozytorium Git.")
   }
-});
+
+  const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const blueTheme = document.querySelector(".js-blueTheme");
+    
+    body.classList.toggle("body--dark");
+
+    if (body.classList.contains("body--dark")) {
+      blueTheme.innerText = "Szary";
+    } else {
+      blueTheme.innerText = "Niebieski";
+    }
+  };
+
+  const init = () => {
+    const button = document.querySelector(".js-changeTheme");
+    button.addEventListener("click", toggleBackground);
+
+    welcome()
+  }
+
+  init()
+
+}
